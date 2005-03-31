7341ae2977ee663aac309c5a0f4f11a14aaf813d
erik <erik@105c9b91-b372-0410-910a-9826ce91b3a7>
2005-03-31 19:25:30 +0000

    Added improved code for adding files to a project.

--- neo/src/Tools/VsTool/CodeGenAdaptor.cs
+++ neo/src/Tools/VsTool/CodeGenAdaptor.cs
@@ -1,30 +1,33 @@
 using System;
 using System.Collections;
-using System.Runtime.InteropServices;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows.Forms;
+using CustomToolGenerator;
 using EnvDTE;
 using Neo.Generator.CodeGen;
+using Neo.MetaModel.Reader;
 
 
 namespace Neo.VsTool
 {
 	[Guid("3A95AC49-6FE5-4841-A12A-16C4FBAFBB8D")]
-	public class CodeGenAdaptor : CustomToolGenerator.BaseCodeGeneratorWithSite
+	public class CodeGenAdaptor : BaseCodeGeneratorWithSite
 	{
 		protected override byte[] GenerateCode(string path, string contents)
 		{
-			StringWriter	writer;
-			CodeGenerator	codeGen;
-			byte[]			output;
+			StringWriter writer;
+			CodeGenerator codeGen;
+			byte[] output;
 
 			writer = new StringWriter();
 			codeGen = new CodeGenerator();
-			codeGen.ReaderType = typeof(Neo.MetaModel.Reader.NorqueReader);
+			codeGen.ReaderType = typeof(NorqueReader);
 
 			try
 			{
-				codeGen.GenerateSupportClasses(path, writer);				
+				codeGen.GenerateSupportClasses(path, writer);
 			}
 			catch(Exception e)
 			{
@@ -33,14 +36,14 @@ namespace Neo.VsTool
 			}
 			try
 			{
-				ProjectItem	modelItem;
-				IList		fileList;
-				
+				ProjectItem modelItem;
+				IList fileList;
+
 				fileList = codeGen.GenerateClassFiles(path, null, true, false);
 				modelItem = base.GetService(typeof(ProjectItem)) as ProjectItem;
 				if(modelItem == null)
 					throw new ApplicationException("Generated/updates files but cannot automatically add new files to project.");
-				foreach(string file in fileList)
+				foreach (string file in fileList)
 					AddFileToProjectIfNecessary(modelItem.ContainingProject, file);
 			}
 			catch(Exception e)
@@ -48,22 +51,70 @@ namespace Neo.VsTool
 				MessageBox.Show(e.Message, "NeoCodeGen");
 			}
 
-			output = System.Text.Encoding.ASCII.GetBytes(writer.ToString());
+			output = Encoding.ASCII.GetBytes(writer.ToString());
 			writer.Close();
-			
+
 			return output;
 		}
 
 
-		private void AddFileToProjectIfNecessary(Project project, string file)
+		private void AddFileToProjectIfNecessary(Project project, string filename)
 		{
-			// We should probably check whether the file needs adding BUT this is not 
-			// trivial (you may have to traverse folders etc.) and the AddFromFile
-			// method in VS.NET 2003 does the right thing as it simply ignores superfluous 
-			// adds. However, there is a remark in the docs saying it should fail if the 
-			// file is in the project already...
-			project.ProjectItems.AddFromFile(file);
+			if(ItemsContainFile(project.ProjectItems, filename) == false)
+				project.ProjectItems.AddFromFile(filename);
 		}
 
+
+		private bool ItemsContainFile(ProjectItems items, string filename)
+		{
+			foreach(ProjectItem projectItem in items)
+			{
+				if(IsFolder(projectItem))
+					return ItemsContainFile(projectItem.ProjectItems, filename);
+				else
+					return ProjectItemRepresentsFile(projectItem, filename);
+			}
+			return false;
+		}
+
+
+		// if match check document full name in case it's the right filename wrong folder
+		// NOTE: accessing the document can cause a COM exception
+		private bool ProjectItemRepresentsFile(ProjectItem projectItem, string filename)
+		{
+			if(filename.ToLower() == projectItem.Name.ToLower())
+			{
+				try
+				{
+					Document doc = projectItem.Document;
+					if(doc != null && doc.FullName != null)
+					{
+						if(doc.FullName.ToLower() == doc.FullName.ToLower())
+							return true;
+					}
+				}
+				catch(COMException)
+				{
+					// ignore it
+				}
+			}
+			return false;
+		}
+
+		public bool IsFolder(ProjectItem item)
+		{
+			string[] folderGUIDs =
+				{
+					"{6BB5F8EF-4483-11D3-8BCF-00C04F8EC28C}", // GUID for VB, C#, and J# folders
+					"{6BB5F8F0-4483-11D3-8BCF-00C04F8EC28C}"  // GUID for VC folders
+				};
+			foreach(string guid in folderGUIDs)
+			{
+				if(item.Kind.ToUpper() == guid)
+					return true;
+			}
+			return false;
+		}
 	}
+
 }
