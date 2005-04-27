a59c59fd29c37001a04cbf8f60ff121db76dd250
erik <erik@105c9b91-b372-0410-910a-9826ce91b3a7>
2005-04-27 14:20:44 +0000

    Removed Console output.

--- neo/src/Neo.MySqlClient/MySqlImplFactory.cs
+++ neo/src/Neo.MySqlClient/MySqlImplFactory.cs
@@ -33,16 +33,13 @@ namespace Neo.MySqlClient
         {
             MySqlDbType     dbtype;
             MySqlParameter  param;
-            Console.Out.WriteLine("pname = {0}", pname);
 
             if((dbtype = ConvertToDbType(pcolumn.DataType)) == MySqlDbType.Blob)
                 throw new ArgumentException(String.Format("Cannot handle data type {0} of column {1} in table {2}.", pcolumn.DataType.ToString(), pcolumn.ColumnName, pcolumn.Table.TableName));
             param = new MySqlParameter(pname, dbtype, Math.Max(0, pcolumn.MaxLength), pcolumn.ColumnName);
-            Console.Out.WriteLine("param.ParameterName = {0}", param.ParameterName);
             param.IsNullable = pcolumn.AllowDBNull;
             param.Value = pvalue;
             param.ParameterName = pname;
-            Console.Out.WriteLine("param.ParameterName = {0}", param.ParameterName);
             return param;
         }
 
