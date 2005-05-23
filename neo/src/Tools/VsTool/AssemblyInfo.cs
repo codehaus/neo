5ae282788bc4be23094dee17395e9714fa1faf2d
poutsma <poutsma@105c9b91-b372-0410-910a-9826ce91b3a7>
2005-05-23 07:40:42 +0000

    Fixed NEO-27.

--- neo/src/Tools/VsTool/AssemblyInfo.cs
+++ neo/src/Tools/VsTool/AssemblyInfo.cs
@@ -1,5 +1,15 @@
 using System;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Neo Visual Studio tool")]
 [assembly: AssemblyDescription("Visual Studio tool for Neo")]
+[assembly : ComVisible(true)]
+[assembly : CLSCompliant(false)]
+[assembly : AssemblyConfiguration("")]
+[assembly : AssemblyCompany("")]
+[assembly : AssemblyProduct("")]
+[assembly : AssemblyCopyright("See http://neo.codehaus.org for details.")]
+[assembly : AssemblyTrademark("")]
+[assembly : AssemblyCulture("")]
+[assembly : AssemblyVersion("1.3.2.*")]
